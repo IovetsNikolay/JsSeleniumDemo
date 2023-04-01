443671d1b0854d755b68effe54ca529a4529ce1c
IovetsNikolay <indicow@gmail.com>
2023-04-01 19:11:07 +0200

    first commit

---
+++ test/firstTest.js
@@ -0,0 +1,27 @@
+const {Builder, By, Key} = require(`selenium-webdriver`);
+const assert = require(`assert`);
+const should = require(`chai`).should();
+
+describe("add todo test", () =>
+    it("success add todo in app", async () => {
+    
+        let driver = await new Builder()
+            .forBrowser("chrome")
+            .build();
+        await driver
+            .get("https://lambdatest.github.io/sample-todo-app/");
+        await driver
+            .findElement(By.id("sampletodotext"))
+            .sendKeys("adasdasdas", Key.RETURN);
+        let todoText = await driver
+            .findElement(By.xpath("//li[last()]"))
+            .getText()
+            .then(value => value);
+        //assert with node assertion
+        assert.strictEqual(todoText, "adasdasdas");
+        //assert using chai
+        todoText.should.equal("adasdasdas")
+
+        await driver.quit();
+    })
+);
